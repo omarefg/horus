ae38eac7deb83dd94f9d9971443f7d5253f18737
Omar Flores Grimontt <dev@omarefg.com>
2020-06-24 07:17:41 -0500

    Started design system foundations

---
+++ src/styles/palette.js
@@ -0,0 +1,5 @@
+export const text = 'rgb(45, 44, 60)';
+export const border = 'rgb(45, 44, 60)';
+export const primary = 'rgb(101, 217, 228)';
+export const background = '#fefefe';
+export const card = '#fff';
---
+++ src/styles/spacing.js
@@ -0,0 +1,5 @@
+const unit = 10;
+
+export function getSpacing(times = 1) {
+  return times * unit;
+}
